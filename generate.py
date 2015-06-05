997eb81941bb4bd7f9ee3b34c5ec2496128c3a0b
Brandon Gomez <brlgomez@ucsc.edu>
2015-06-04 22:15:10 -0700

    made it have more interesting results

--- generate.py
+++ generate.py
@@ -8,11 +8,11 @@ from random import random, randint, choice
 
 Library = {
   'Initial': {
-    'width': 20,
-    'length': 10,
+    'width': 30,
+    'length': 20,
     'body':{}
     },
-  'Types': ["a", "b", "c", "d","f","g","h","i","j","k","l"],
+  'Types': ["a", "b", "c", "d","f","g","h"],
   'Props': {
     "a" : {
       'sword': 2,
@@ -30,6 +30,26 @@ Library = {
       'fire': 1
       },
     "d" : {
+      'sword': 1,
+      'arrow': 4,
+      'fire': 1
+      },
+    "e" : {
+      'sword': 2,
+      'arrow': 1,
+      'fire': 0
+      },
+    "f" : {
+      'sword': 0,
+      'arrow': 0,
+      'fire': 3
+      },
+    "g" : {
+      'sword': 1,
+      'arrow': 1,
+      'fire': 1
+      },
+    "h" : {
       'sword': 1,
       'arrow': 4,
       'fire': 1
@@ -74,21 +94,21 @@ def createMonster(parts):
   monsterShape.insert(-1, [width/2, length/2])
   next = []
   j = 0
-  for j in range(0, monsterLength-1):
+  for j in range(0, monsterLength - 1):
     i = 0
     for i in range(0, len(monsterShape)):
       x = monsterShape[i][0]
       y = monsterShape[i][1]
-      if[x - 0, y + 1] not in monsterShape: 
+      if[x - 0, y + 1] not in monsterShape and [x - 0, y + 1] not in next: 
         if x-0 >= 0 and x-0 <= width and y+1 >= 0 and y+1 <= length:
           next.insert(-1,[x - 0, y + 1])
-      if[x - 1, y - 0] not in monsterShape:
+      if[x - 1, y - 0] not in monsterShape and [x - 1, y - 0] not in next:
         if x-1 >= 0 and x-1 <= width and y+0 >= 0 and y+0 <= length:
           next.insert(-1,[x - 1, y - 0])
-      if[x - 0, y - 1] not in monsterShape:
+      if[x - 0, y - 1] not in monsterShape and [x - 0, y - 1] not in next:
         if x-0 >= 0 and x-0 <= width and y-1 >= 0 and y-1 <= length:
           next.insert(-1,[x - 0, y - 1])
-      if[x + 1, y - 0] not in monsterShape:
+      if[x + 1, y - 0] not in monsterShape and [x + 1, y - 0] not in next:
         if x+1 >= 0 and x+1 <= width and y-0 >= 0 and y-0 <= length:
           next.insert(-1,[x + 1, y - 0])
     monsterShape.insert(-1, next.pop(randint(0, len(next) - 1)))
@@ -96,7 +116,8 @@ def createMonster(parts):
   return monsterShape
 
 monster = []
-monster = createMonster(20)
+monster = createMonster(50)
+
 state = Library['Initial']
 for j in range(0, len(monster)):
   assign(state, monster[j][0], monster[j][1])
